22dea21f0b2fe0d6d56925c40b165f6d0b29deee
Derrick Timmermans <derrick.timmermans@outlook.com>
2024-07-29 11:21:54 +0200

    Implement search functionality

--- src/GUI/Groups.ts
+++ src/GUI/Groups.ts
@@ -1,34 +1,67 @@
 // Generate dynamically group panels in the GroupsPage.html
-import { fetchGroups } from './api';
+import { fetchGroups, type GroupSummary } from './api';
 
-async function updateGroups() {
-    console.log("I'm in functions");
+var groups: GroupSummary[] = [];
+var searchBarContent: string = "";
+
+async function init() {
+    const searchBar = document.getElementById("groupsSearch");
+    searchBar?.addEventListener("input", updateSearch);
+
+    refreshGroupData();
+}
+
+async function refreshGroupData() {
+    groups = await fetchGroups(); // Fetch user data
+    updateGroupsDisplay();
+}
+
+async function updateSearch(event: Event) {
+    const target = event.target as HTMLInputElement;
+
+    searchBarContent = target.value;
+    updateGroupsDisplay();
+}
+
+function filterGroups() {
+    return groups.filter(
+        g =>
+            g.robotID.matchFuzzy(searchBarContent)
+            || g.names.some(n => n.matchFuzzy(searchBarContent))
+    );
+}
+
+async function updateGroupsDisplay() {
     const contentDiv = document.getElementById('creatingGroupPanels');
 
     if (!contentDiv)
         return;
 
-    const users = await fetchGroups(); // Fetch user data
-    if (users.length === 0) {
+    if (groups.length === 0) {
         contentDiv.innerHTML = "<h2>No groups are active at this moment.</h2>";
         return;
     }
 
+    const filteredGroups = filterGroups();
+
     var newChildren: Node[] = [];
 
     // Loop through all the groups 0 - n
-    users.forEach((user) => {
+    filteredGroups.forEach(group => {
         const groupCard = document.createElement('a');
         groupCard.classList.add('card', "groupCard");
-        groupCard.id = user.robotID;
-        groupCard.href = `IndividualGroup.html?robotID=${encodeURIComponent(user.robotID)}`;
-
+        groupCard.id = group.robotID;
+        groupCard.href = `IndividualGroup.html?robotID=${encodeURIComponent(group.robotID)}`;
 
         const robotNameTag = document.createElement('h1');
-        robotNameTag.innerHTML = user.robotID;
+        robotNameTag.innerHTML = group.robotID;
 
         const usersList = document.createElement('p');
-        usersList.innerHTML = user.names.join('<br>');
+
+        if (group.names.length == 0)
+            usersList.innerHTML = "No are to players";
+        else
+            usersList.innerHTML = group.names.join('<br>');
 
         groupCard.appendChild(robotNameTag);
         groupCard.appendChild(usersList);
@@ -39,6 +72,35 @@ async function updateGroups() {
     contentDiv.replaceChildren(...newChildren);
 }
 
-updateGroups();
-setInterval(updateGroups, 5000);
+
+
+// Simple fuzzy search implementation by Trevor Dixon
+// Source: https://stackoverflow.com/a/16908326
+function matchFuzzy(this: string, search: string) {
+    search = search.toUpperCase();
+    const text = this.toUpperCase();
+
+    var j = -1; // remembers position of last found character
+
+    // consider each search character one at a time
+    for (var i = 0; i < search.length; i++) {
+        var l = search[i];
+        if (l == ' ') continue;     // ignore spaces
+
+        j = text.indexOf(l, j + 1);     // search for character & update position
+        if (j == -1) return false;  // if it's not found, exclude this item
+    }
+    return true;
+}
+
+// Declare the Extension
+declare global {
+    interface String {
+        matchFuzzy(search: string): boolean;
+    }
+}
+String.prototype.matchFuzzy = matchFuzzy;
+
+init();
+setInterval(refreshGroupData, 5000);
 
