6090f0d12c455eb71a710f30099ee7f12b2cd381
Derrick Timmermans <derrick.timmermans@outlook.com>
2024-07-30 11:18:24 +0200

    Actually copy static stuff to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -45,6 +46,11 @@ module.exports = {
             filename: 'IndividualGroup.html',
             chunks: ['IndividualGroup'],
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'static' }
+            ]
+        })
     ],
     devServer: {
         headers: {
